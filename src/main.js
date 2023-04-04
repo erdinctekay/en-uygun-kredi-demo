cf10c94a071e05b91127a609f873ddc667d4e7c0
Erdinc Tekay <erdinctekay@gmail.com>
2023-04-05 02:53:41 +0300

    added dom load event listener

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-import './style.css'
-
+import '@/style.css'
+import { loadUiFunctions } from '@/helpers/ui'
 import App from '@/App'
 
 const mountTarget = document.querySelector('#app')
+const body = document.querySelector('body')
 // execute app on related dom element
 App(mountTarget)
+
+document.addEventListener('DOMContentLoaded', () => {
+	loadUiFunctions()
+	setTimeout(() => {
+		document.querySelector('body').style.opacity = '' // to show body (initial opacity was 0)
+	}, 100)
+})
