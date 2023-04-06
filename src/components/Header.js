a2f621c1674825054701ab5c213788ee163e86f1
Erdinc Tekay <erdinctekay@gmail.com>
2023-04-06 16:38:15 +0300

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,9 @@ export default function Header() {
                     class="navbar navbar-expand-lg p-0 w-100 bg-gray-800"
                 >
                     <div class="container-fluid container-fluid-xxl py-2 px-3 px-sm-5 row g-0 m-auto">
-                        <div class="d-flex flex-column w-100 py-3">
-                            <span class="lead fw-bold fst-italic">En Uygun Kredi</span>
+                        <div class="d-flex flex-column w-100 py-3 pb-2">
+                            <span class="lead fw-bold text-center">EN UYGUN KREDİ</span>
+                            <span class="small text-center"><a class="text-muted" href="https://github.com/erdinctekay/en-uygun-kredi-demo" target="_blank">source code</a></span>
                         </div>
                     </div>
                 </nav>
